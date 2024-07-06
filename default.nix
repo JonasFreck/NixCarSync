56959847a6f2d2e3ac3103871ae1708451318b31
JonasF <jonasfreck@gmail.com>
2024-07-06 17:38:28 -0400

    trying to get environmental vars to work

--- default.nix
+++ default.nix
@@ -1,11 +1,16 @@
 { pkgs ? import <nixpkgs> { overlays = [(import ./overlay.nix)]; } }:
 
+
 pkgs.mkShell {
+
+  
   buildInputs = [
     pkgs.myEnv
   ];
 
   shellHook = ''
+    export SOURCE_PATH="source/directory"
+    export DESTINATION_PATH="destination/directory"
     echo "Environment with Python and rsync set up. You can run your script now."
   '';
 }
